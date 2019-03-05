1b1cca356a1eaaf2b966b539c80cef4c4e5191b3
Danny Takeuchi <dannytakeuchi@DN525b3c.SUNet>
2019-03-04 21:01:29 -0800

    Adjusted eval set

--- debug_squad/eval.py
+++ debug_squad/eval.py
@@ -1,16 +1,31 @@
 import json
+def writeCleanDevJson():
+    with open('dev-v2.0.json') as f:
+        data = json.load(f)
 
-with open('dev-v2.0.json') as f:
-    data = json.load(f)
-
-g = open("dev-2.0-cleaned.txt", "w")
+    g = open("dev-2.0-cleaned.txt", "w")
 
 
-data = data['data']
-for y in data:
-    questions =y['paragraphs']
-    for q in questions:
-        qas = q['qas']
-        for a in qas:
-            answers = a['answers']
-            g.write(str(answers) + '\n')
+    data = data['data']
+    for y in data:
+        questions =y['paragraphs']
+        for q in questions:
+            qas = q['qas']
+            for a in qas:
+                answers = a['answers']
+                g.write(str(answers) + '\n')
+
+
+def jsonToCSV():
+    import csv
+    # Write submission file for Kaggle
+    with open('dev_submission.csv', 'w') as csv_fh:
+        csv_writer = csv.writer(csv_fh, delimiter=',')
+        csv_writer.writerow(['Id', 'Predicted'])
+
+        with open('predictions.json') as f:
+            data = json.load(f)
+            for uuid in data:
+                csv_writer.writerow([uuid, data[uuid].encode('utf-8')])
+
+jsonToCSV()
