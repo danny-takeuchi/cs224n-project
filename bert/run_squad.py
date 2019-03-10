99017c8a3b5c2b0839bebc4c5076bf504026a4e6
Ubuntu <dtkt@bert-base.yvckph3fla3ergi405favdztld.bx.internal.cloudapp.net>
2019-03-10 02:07:05 +0000

    BERT highway

--- bert/run_squad.py
+++ bert/run_squad.py
@@ -841,7 +841,7 @@ def main():
     parser.add_argument('--version_2_with_negative',
                         action='store_true',
                         help='If true, the SQuAD examples contain some that do not have an answer.')
-    parser.add_argument('--null_score_diff_thrBertForQuestionAnsweringWHLHighwayeshold',
+    parser.add_argument('--null_score_diff_threshold',
                         type=float, default=0.0,
                         help="If null_score - best_non_null is greater than the threshold predict null.")
     parser.add_argument('--improvement',
